ce8a46d3ed734e2b6a3a9c2ae96c0a5ee9d21780
Fedor Nikolayev <fnikolay@ucsc.edu>
2015-02-02 21:22:31 -0800

    added problem 9
    ...

--- hw3.pl
+++ hw3.pl
@@ -1,9 +1,16 @@
-/**Problem 7: */
+/** Problem 7: */
 father(al, bud).
 father(al, kelly).
 mother(peggy, kelly).
 mother(peggy, bud).
 mother(martha, peggy).
 
-/**Problem 8: */
-grandma(X,Y) :-mother(X, Z),(mother(Z, Y);father(Z, Y)).
+
+/** Problem 8: */
+%parent defines a child of a mother or father
+parent(X,Y) :- (father(X, Y) ; mother(X, Y)).
+grandma(X,Y) :- mother(X, Z) , parent(Z, Y).
+
+/** Problem 9: */
+descendants(X,Y) :- parent(X, Z) , descendants(Z, Y).
+descendants(X,Y) :- parent(X, Y).
